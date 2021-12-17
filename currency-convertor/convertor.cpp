604a4d4990054450ed467df62711014f6a7abc77
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2021-12-17 13:44:58 +0200

    deleted some comments

--- currency-convertor/convertor.cpp
+++ currency-convertor/convertor.cpp
@@ -2,16 +2,6 @@
 #include <cstring>
 #include <cctype>
 
-
-//int main()
-//{
-//    std::cout << "Enter a number: "; // просим пользователя ввести любое число
-//    int a = 0;
-//    std::cin >> a; // получаем пользовательское число и сохраняем его в переменную a
-//    std::cout << "You entered " << a << std::endl;
-//    return 0;
-//}
-
 using namespace std;
 
 string toUppercase(string str){
