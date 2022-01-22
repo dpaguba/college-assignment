7e4caa551081eeba65270cb4e36c6ff7d20b57e7
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-01-22 20:02:43 +0100

    make the structure for program to handle users input

--- binary-calculator/BinaryCalculator.java
+++ binary-calculator/BinaryCalculator.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 public class BinaryCalculator {
 
-    
+    public static void main(String[] args) {
+
+        System.out.println("Welcome in binary number calculator!");
+        System.out.println("Please enter numbers with the same quantity of digits, so I can work properly!\n");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your first binary number: ");
+        String firstInput = in.next();
+        System.out.println("Enter your second binary number: ");
+        String secondInput = in.next();
+        System.out.println("What should I do? (+, -, *): ");
+        String sign = in.next();
+
+        switch (sign){
+            case "+":
+                // do addition
+                break;
+            case "-":
+                //do substraktion
+                break;
+            case "*":
+                //do multiplikation
+                break;
+            default:
+                System.out.println("Invalid Input");
+                break;
+        }
+
+    }
 
 
 }
+
 
