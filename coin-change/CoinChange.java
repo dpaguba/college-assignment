4be609abfa3741574f9775ecf47618f045ced0d5
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-01-24 09:50:53 +0100

    Update CoinChange.java

--- coin-change/CoinChange.java
+++ coin-change/CoinChange.java
@@ -1,2 +1,15 @@
 public class CoinChange {
+  
+  public static void main(String[] args) {
+
+        try{
+            /*
+            Erste if-Schleife ueberprueft, ob die richtige Parameteranzahl geliefert wird. Falls nicht, greift die
+            Ausnahme in der Zeile 56.
+             */
+            if (args.length == 2){
+                //Variable fuer die Waehrung
+                String currency = args[0];
+                //Variable fuer das Wechselgeld
+                int change;
 }
