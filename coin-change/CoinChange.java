66f3c32f62e986caef12d5aee1e7de407100851e
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-01-24 09:56:03 +0100

    completed

--- coin-change/CoinChange.java
+++ coin-change/CoinChange.java
@@ -5,14 +5,14 @@ public class CoinChange {
         try{
             /*
             Erste if-Schleife ueberprueft, ob die richtige Parameteranzahl geliefert wird. Falls nicht, greift die
-            Ausnahme in der Zeile 56.
+            Ausnahme
              */
             if (args.length == 2){
                 //Variable fuer die Waehrung
                 String currency = args[0];
                 //Variable fuer das Wechselgeld
                 int change;
-              // ueberpruefen, ob der int-Wert uebergegeben wird. Falls nicht, wirft die Ausnahme in der Zeile 21.
+              // ueberpruefen, ob der int-Wert uebergegeben wird. Falls nicht, wirft die Ausnahme
                 try {
                     //wandelt String-Wert in int-Wert
                     change = Integer.parseInt(args[1]);
@@ -22,9 +22,9 @@ public class CoinChange {
                     return;
                 }
 
-                // ueberprueft, ob die bekannte Waehrung angegeben wird. Falls nicht, wirft die Ausnahme in der Zeile 59.
+                // ueberprueft, ob die bekannte Waehrung angegeben wird. Falls nicht, wirft die Ausnahme
                 if (currency.equals("Euro") || currency.equals("Mira")){
-                  //ueberprueft, ob der fuers Wechselgeld keine negative Zahl ist. Falls nicht, wirft die Ausnahme in der Zeile 45.
+                  //ueberprueft, ob der fuers Wechselgeld keine negative Zahl ist. Falls nicht, wirft die Ausnahme
                     if (change >= 0){
                         // betrachtet man der Fall, wenn der Wert Euro als Waehrung uebergegeben wird.
                         if (currency.equals("Euro")){
