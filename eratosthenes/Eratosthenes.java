4c746d4cd4bcee21ab4b3f6a798a846633073dd5
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-01-26 23:00:40 +0100

    Update Eratosthenes.java

--- eratosthenes/Eratosthenes.java
+++ eratosthenes/Eratosthenes.java
@@ -1,1 +1,14 @@
-
+public class Eratosthenes {
+    public static void main(String[] args) throws Exception{
+        // Zuerst wird erwartet, dass die Parameteranzahl der vorgesehenen entspricht.
+        try {
+            /*
+            Zuerst betrachtet man den Fall, wenn nur eine Zahl uebergegeben werden soll.
+            Wenn's nicht der Fall ist, wird die Fehlermeldung(Zeile 63 - 65) ausgegeben
+             */
+            if (args.length == 1){
+                /*
+                Die Gueltigkeit der uebergegebenen Parameter laesst sich durch dieses try-catch Block ueberpruefen.
+                 */
+                try {
+                  
