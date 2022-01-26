36c72eaa00588671c91a14f2d907a8b4fdb78ab7
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-01-26 23:00:47 +0100

    Update Eratosthenes.java

--- eratosthenes/Eratosthenes.java
+++ eratosthenes/Eratosthenes.java
@@ -11,4 +11,19 @@ public class Eratosthenes {
                 Die Gueltigkeit der uebergegebenen Parameter laesst sich durch dieses try-catch Block ueberpruefen.
                  */
                 try {
+                    /*
+                    Falls der Parameter zum Integer-Wert nicht umgewandelt werden kann,
+                    erwartet man hier Fehlermeldung (Zeile 29 - 30)
+                     */
+                    int point = Integer.parseInt(args[0]);
+                    /*
+                    Als Primzahlen wird in unserem Fall nur die Menge der natuerlichen Zahlen betrachtet.
+                    Hat der Parameter einen negativen Wert, wird die Fehlermeldung (Zeile 29 - 30) ausgegeben.
+                    Somit betrachten wir nur die positiven Zahlen.
+                     */
+                    if (point > 0){
+                        System.out.print(countPrim(point));
+                    }else {
+                        throw new IllegalArgumentException();
+                    }
                   
