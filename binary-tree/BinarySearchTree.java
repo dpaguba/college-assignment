f415dd7c7e67e4f145244c15983443aff7256616
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-02-03 10:22:48 +0100

    do ex. 1

--- binary-tree/BinarySearchTree.java
+++ binary-tree/BinarySearchTree.java
@@ -3,23 +3,29 @@ import java.util.Iterator;
 public class BinarySearchTree<T extends Comparable<T>>
 {
 
-  public int mostTwoChildrenNodes()
-  {
-      if (isEmpty()){
-          return 0;
-      }else{
-          if (isLeaf()){
-              return 0;
-          }
-          if (!rightChild.isEmpty() && !leftChild.isEmpty()){
-              return 1+Math.max(rightChild.mostTwoChildrenNodes(), leftChild.mostTwoChildrenNodes());
-          }else if (rightChild.isEmpty() && !leftChild.isEmpty()){
-              return leftChild.mostTwoChildrenNodes();
-          }else {
-              return rightChild.mostTwoChildrenNodes();
-          }
+  /*
+  Excercise 1
+  */
+    public int countNodes (int top, int bottom){
+        if (top<=bottom && !isEmpty()){
+            if (top<=0 && bottom>=0){
+                return 1
+                        + leftChild.countNodes(top-1, bottom-1)
+                        + rightChild.countNodes(top-1, bottom-1);
+            }
+            if(bottom < 0){
+                return 0;
+            }
+            return leftChild.countNodes(top-1, bottom-1)
+                    + rightChild.countNodes(top-1, bottom-1);
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    
+    
 
-      }
-
-  }
+}
   
