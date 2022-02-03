d796bbbf7c9d2ff437cd2dcfe1cee6d8dd9c3689
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-02-03 10:20:31 +0100

    mostTwoChildrenNodes

--- binary-tree/BinarySearchTree.java
+++ binary-tree/BinarySearchTree.java
@@ -1,1 +1,25 @@
+import java.util.Iterator;
 
+public class BinarySearchTree<T extends Comparable<T>>
+{
+
+  public int mostTwoChildrenNodes()
+  {
+      if (isEmpty()){
+          return 0;
+      }else{
+          if (isLeaf()){
+              return 0;
+          }
+          if (!rightChild.isEmpty() && !leftChild.isEmpty()){
+              return 1+Math.max(rightChild.mostTwoChildrenNodes(), leftChild.mostTwoChildrenNodes());
+          }else if (rightChild.isEmpty() && !leftChild.isEmpty()){
+              return leftChild.mostTwoChildrenNodes();
+          }else {
+              return rightChild.mostTwoChildrenNodes();
+          }
+
+      }
+
+  }
+  
