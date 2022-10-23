36de1ee35b1c575bac7a4b7b79de59ef03420a9f
Dmytro Pahuba <dpaguba@gmail.com>
2022-10-24 00:00:00 +0200

    Daily commit: Card.kt

--- sopra/swim/src/main/koltin/entity/Card.kt
+++ sopra/swim/src/main/koltin/entity/Card.kt
@@ -10,3 +10,5 @@ data class Card(val suit: CardSuit, val value: CardValue) {
     /** provide two characters to represent card's value. */
     override fun toString() = "$suit$value"
 }
+
+# Modified 2025-08-11 10:24:33
