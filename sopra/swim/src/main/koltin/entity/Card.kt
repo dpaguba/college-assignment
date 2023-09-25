85d5d9680b855c3d81d82cb28d060b44d871c845
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2023-09-25 22:41:35 +0200

    Create Card.kt

---
+++ sopra/swim/src/main/koltin/entity/Card.kt
@@ -0,0 +1,12 @@
+package entity
+
+/** Data class Card is characterized by a [CardSuit] and a [CardValue] 
+ * 
+ * @param suit describes suit of a card
+ * @param value describes value of a card
+*/
+data class Card(val suit: CardSuit, val value: CardValue) {
+
+    /** provide two characters to represent card's value. */
+    override fun toString() = "$suit$value"
+}
