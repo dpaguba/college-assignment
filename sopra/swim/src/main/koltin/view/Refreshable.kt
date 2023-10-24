5dded65804882b6346d5641685a646c9e04b89a7
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2023-10-24 11:09:24 +0200

    Create Refreshable.kt

---
+++ sopra/swim/src/main/koltin/view/Refreshable.kt
@@ -0,0 +1,21 @@
+package view
+
+interface Refreshable {
+
+    /** perform refreshes that are necessary when game ended and end results have to be displayed */
+    fun refreshAfterGameEnd() {}
+
+    /** perform refreshes that are necessary after one or all cards were swapped */
+    fun refreshAfterCardChange() {}
+
+    /** perform refreshes that are necessary after turn goes to next player */
+    fun refreshAfterTurn() {}
+
+    /**
+     * perform refreshes that are necessary after player was created and added to the player list
+     */
+    fun refreshAfterCreatePlayer() {}
+
+    /** perform refreshes after game was started */
+    fun refreshAfterStartGame() {}
+}
