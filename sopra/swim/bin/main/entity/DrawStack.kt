c6407954cfa93756edb221f4a9a2ccba81d05a93
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2023-12-04 00:09:55 +0100

    feat: create drawStack

---
+++ sopra/swim/bin/main/entity/DrawStack.kt
@@ -0,0 +1,10 @@
+package entity
+
+/**
+ * Data class DrawStack is characterized by a [CardSuit] and a [CardValue] and stores information
+ * about cards in draw pile
+ */
+data class DrawStack(val suit: CardSuit, val value: CardValue) {
+
+    var drawStack: MutableList<Card> = ArrayList(32) // rename into drawPile
+}
