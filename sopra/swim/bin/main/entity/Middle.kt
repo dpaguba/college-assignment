9fb10db760d41cfc349ee3ec2b393c3d2c3e3206
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2023-12-05 10:49:38 +0100

    feat: create middle.kt

---
+++ sopra/swim/bin/main/entity/Middle.kt
@@ -0,0 +1,12 @@
+package entity
+
+import kotlin.collections.mutableListOf
+
+/**
+ * Data class DrawStack is characterized by a [CardSuit] and a [CardValue] and stores information
+ * about cards on the table
+ */
+data class Middle(val suit: CardSuit, val value: CardValue) {
+
+    var middle: MutableList<Card> = mutableListOf() // rename into tableCards
+}
