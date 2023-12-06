8caf0be2ffe319ccf9a0040b6e34a419aab7d2d2
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2023-12-07 00:39:23 +0100

    feat: create swim

---
+++ sopra/swim/bin/main/entity/Swim.kt
@@ -0,0 +1,11 @@
+package entity
+
+/**
+ * Data class that represents a game state of Swim. and stores information about amount of passes,
+ * players.
+ */
+data class Swim(
+        var passCounter: Int = 0,
+        var currentPlayer: Player = Player("Nomen Nominandum"),
+        var players: MutableList<Player> = mutableListOf(),
+)
