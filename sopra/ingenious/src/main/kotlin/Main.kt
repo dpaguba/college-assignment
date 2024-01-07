eac4a844fc66b26de137e0ebc1795a8fcd3e9641
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2024-01-07 18:56:14 +0100

    feat: create Main.kt

---
+++ sopra/ingenious/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import view.Application
+
+/**
+ * The main class of Ingenious, which only calls the Application
+ */
+fun main() {
+    println("Application start")
+    Application().show()
+    println("Application ended. Goodbye")
+}
