7377c7cda730cbd4304975a15c3a7403fbb88ce0
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2024-01-21 03:10:19 +0200

    feat: create connection state

---
+++ sopra/ingenious/src/main/kotlin/service/ConnectionState.kt
@@ -0,0 +1,16 @@
+package service
+
+/**
+ * A class that contains all current States that are important for the Logic
+ * //might be deleted
+ */
+enum class ConnectionState {
+    DISCONNECTED,
+    CONNECTED,
+    WAIT_FOR_HOST_CONFIRMATION,
+    WAIT_FOR_PLAYER_JOIN,
+    WAIT_FOR_JOIN_CONFIRMATION,
+    WAIT_FOR_GAME_INIT,
+    PLAYING_MY_TURN,
+    WAITING_FOR_OPPONENT_TURNS
+}
