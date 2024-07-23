f8cc33b82fdaebb9cfa992f4b68bb8ff3107b20b
Dmytro Pahuba <dpaguba@gmail.com>
2024-07-23 02:12:47 +0200

    feat: Add PlaybackClientActor class for managing playback of songs

---
+++ mnp/assignment/akkablanko/src/main/java/com/example/PlaybackClientActor.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.*;
+
+import java.time.Duration;
+
+
+public class PlaybackClientActor extends AbstractBehavior<PlaybackClientActor.Message> {
+
+    public interface Message {}
+
+    public record Init(ActorRef<QueueManagerActor.Message> queueManagerActor) implements Message {}
+
+    public record PlayMessage(String artist, String title, int duration) implements Message {
+        public String getTitle() {
+            return title;
+        }
+
+        public String getArtist() {
+            return artist;
+        }
+
+        public int getDuration() {
+            return duration;
+        }
+    }
+
+
+    public static Behavior<Message> create() {
+        return Behaviors.setup(context -> Behaviors.withTimers(timers -> new PlaybackClientActor(context, timers)));
+    }
+
+    private final TimerScheduler<PlaybackClientActor.Message> timers;
+
+    private PlaybackClientActor(ActorContext<Message> context, TimerScheduler<PlaybackClientActor.Message> timers) {
+        super(context);
+        this.timers = timers;
+        Message msg = new PlayMessage("", "song", 10);
+        this.timers.startSingleTimer(msg, msg, Duration.ofSeconds(10));
+    }
+
+    @Override
+    public Receive<Message> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(PlayMessage.class, this::onPlayMessage)
+                .build();
+    }
+
+    private Behavior<Message> onPlayMessage(PlayMessage msg) {
+
+        getContext().getLog().info("Done");
+
+        getContext().getSelf().tell(new QueueManagerActor.ReadyMessage(this.getContext().getSelf()));
+
+        return this;
+    }
+}
