bfe56f4871638348e510dbaf4aa9e5b078ecaf5d
Dmytro Pahuba <dpaguba@gmail.com>
2024-07-24 12:02:04 +0200

    feat: Add QueueManagerActor class for managing the playlist queue

---
+++ mnp/assignment/akkablanko/src/main/java/com/example/QueueManagerActor.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class QueueManagerActor extends AbstractBehavior<QueueManagerActor.Message> {
+
+    private final Queue<Song> playlist;
+
+    public interface Message {
+    }
+
+    public record Init(ActorRef<PlaybackClientActor.Message> playbackClientActor) implements Message {}
+
+    public record ReadyMessage (ActorRef<PlaybackClientActor.Message> replyTo) implements Message, PlaybackClientActor.Message { }
+
+    public static class AddMessage implements QueueManagerActor.Message {
+        public String songName;
+        public int duration;
+        public String artist;
+    }
+//
+//    public record CreateMessage(ActorRef<AkkaMainSystem.Create> someReference) implements QueueManagerActor.Message {
+//    }
+
+    public static Behavior<Message> create() {
+        return Behaviors.setup(QueueManagerActor::new);
+    }
+
+    private QueueManagerActor(ActorContext<Message> context) {
+        super(context);
+        this.playlist = new LinkedList<>();
+    }
+
+    @Override
+    public Receive<QueueManagerActor.Message> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(ReadyMessage.class, this::onReadyMessage)
+                .onMessage(AddMessage.class, this::onAddMessage)
+                .build();
+    }
+
+    private Behavior<Message> onReadyMessage(ReadyMessage msg) {
+        if (!this.playlist.isEmpty()) {
+            Song firstSong = playlist.poll();
+            msg.replyTo.tell(new PlaybackClientActor.PlayMessage(
+                    firstSong.getArtistName(),
+                    firstSong.getTitle(),
+                    firstSong.getDuration()
+            ));
+        }
+        return this;
+    }
+
+    private Behavior<Message> onAddMessage(AddMessage msg) {
+        if (!this.playlist.isEmpty()) {
+//            this.playlist.add(msg.songName);
+        } else {
+//            getContext().getSelf().tell(new PlaybackClientActor.SongMessage(msg.songName));
+        }
+        getContext().getLog().info("");
+        return this;
+    }
+}
